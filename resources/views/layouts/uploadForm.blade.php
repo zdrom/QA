7fb363bdaaa76585ddcdd16ccbe78385991f2b76
= <=>
2017-09-26 10:53:50 -0400

    Additional UI Improvements

--- resources/views/layouts/uploadForm.blade.php
+++ resources/views/layouts/uploadForm.blade.php
@@ -1,10 +1,8 @@
-<div class="row justify-content-center">
+<div class="row justify-content-center mt-3">
 	<div class="col-lg-6">
 		<div class="card">
-		  <div class="card-header">
-		    Upload a Campaign Export
-		  </div>
 		  <div class="card-block">
+		  	<h4 class="card-title">Upload a Campaign Export</h4>
 		    <form method="POST" action="/create" enctype='multipart/form-data'>
 		    	{{ csrf_field() }}
 
@@ -12,7 +10,7 @@
 					<input type="file" name="campaign">
 				</div>
 				<div class="form-group mb-0">
-					<button class="btn btn-primary" type="submit">Upload Export</button>
+					<button class="btn btn-outline-secondary" type="submit">Upload Export</button>
 				</div>
 		    </form>
 		  </div>
